4978eadb837ea0b23d99072e73b226a4a0782838
Aditya R. Prajapati <aditya12398@gmail.com>
2019-11-11 00:44:56 +0530

    Basic structure of the code expanded

--- moc-non-simple.cpp
+++ moc-non-simple.cpp
@@ -3,16 +3,35 @@
 
 #include<iostream>
 #include<cmath>
+#include<string>
 
 struct characteristic_lines
 {
     int index;
-    double slope, u, a, J;
+    double slope, u, T, a, p, J;
 };
 
 void main()
 {
-    void write_gnuplot(double x, double t, double u, double a);
-    void compute_point(int i);
-    
+    struct characteristic_lines *incident_line, *reflected_line;
+    void write_gnuplot(double, double, double, double);
+    void compute_point(int);
+    int n;
+
+    //Basic Thermodynamic parameters
+    double p3_p4 = 0.2, gamma = 1.4, R = 287.00;
+    double u3, T_ref = 300, a_ref;
+    double u, a, T, p;
+    std::string filename;
+    std::cout << "Enter the number of characteristic lines needed: ";
+    std::cin >> n;
+
+    //Generating n characteristic lines
+    incident_line=new characteristic_lines[n];
+    reflected_line=new characteristic_lines[n];
+
+    a_ref = sqrt(gamma * R * T_ref);
+    u3 = (2 * a_ref / (gamma - 1)) * (1 - pow(p3_p4, (2 * gamma / (gamma - 1))));
+    /*u = 2 / (gamma + 1) * (a_ref + x /t); //x and t yet to be defined. Syntax error inevitable until resolved; x = 0 refers to wall
+    T = T_ref * (1 - 0.5 * (gamma -1) * pow((u / a_ref), 2));*/
 }
